7a5939c902f6785f86c9fcfe79026bc973a5a084
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:58:56 +0300

    add the class Personne

--- src/main/java/HistoireNotes/Personne.java
+++ src/main/java/HistoireNotes/Personne.java
@@ -1,4 +1,21 @@
 package HistoireNotes;
 
-public class Personne {
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public abstract class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String email;
+    private String telephone;
 }
