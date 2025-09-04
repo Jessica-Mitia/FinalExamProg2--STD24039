8fd969b2182eb391b86bc8b15a015adacf7e9842
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:59:40 +0300

    add the class Examen

--- src/main/java/HistoireNotes/Examen.java
+++ src/main/java/HistoireNotes/Examen.java
@@ -1,4 +1,36 @@
 package HistoireNotes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Examen {
+    private int id;
+    private String titre;
+    private Cours cours;
+    private LocalDateTime dateTime;
+    private int coefficient;
+
+    public double getExamGrade (Examen examen, Etudiant etudiant, Instant t) {
+        Note required_note = null;
+        for (Note note : etudiant.getNotes()) {
+            if (note.getExamen() == examen) {
+                required_note = note;
+            }
+        }
+        assert required_note != null;
+        return required_note.getChangementNotes().stream()
+                .filter(changementNote -> changementNote.getDate().isBefore(t))
+                .max(Comparator.comparing(ChangementNote::getDate))
+                .map(ChangementNote::getNote)
+                .get();
+    }
 }
