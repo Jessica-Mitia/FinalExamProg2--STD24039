5de9344b6792583ab4e3e637787a14e2f967b79d
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:58:37 +0300

    add the class Tuteur

--- src/main/java/HistoireNotes/Tuteur.java
+++ src/main/java/HistoireNotes/Tuteur.java
@@ -1,4 +1,19 @@
 package HistoireNotes;
 
-public class Tuteur {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+
+public class Tuteur extends Personne {
+    String lien;
+
+    public Tuteur(int id, String nom, String prenom, LocalDate dateNaissance, String email, String telephone, String lien) {
+        super(id, nom, prenom, dateNaissance, email, telephone);
+        this.lien = lien;
+    }
 }
