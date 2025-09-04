c742dbc556c15eec6285c9827de3def31e0e372f
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 10:00:30 +0300

    add the class ChangementNote

--- src/main/java/HistoireNotes/ChangementNote.java
+++ src/main/java/HistoireNotes/ChangementNote.java
@@ -1,4 +1,17 @@
 package HistoireNotes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class ChangementNote {
+    private double note;
+    private Instant date;
+    private String raison;
 }
