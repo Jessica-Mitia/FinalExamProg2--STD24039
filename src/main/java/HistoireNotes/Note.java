f603e8d80269be32b9e904049870f9f2156f515c
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:59:17 +0300

    add the class Note

--- src/main/java/HistoireNotes/Note.java
+++ src/main/java/HistoireNotes/Note.java
@@ -1,4 +1,17 @@
 package HistoireNotes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Note {
+    private int id;
+    private Examen examen;
+    private List<ChangementNote> changementNotes;
 }
