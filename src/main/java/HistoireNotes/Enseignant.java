8b0b258b55fdded7814be94b47eb22b700b95eeb
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:58:09 +0300

    add the class Enseignant

--- src/main/java/HistoireNotes/Enseignant.java
+++ src/main/java/HistoireNotes/Enseignant.java
@@ -1,4 +1,22 @@
 package HistoireNotes;
 
-public class Enseignant {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class Enseignant extends Personne{
+    public enum Specialite {
+        FRONTEND, BACKEND
+    }
+
+    private Specialite specialite;
+
+    public Enseignant(int id, String nom, String prenom, LocalDate dateNaissance, String email, String telephone, Specialite specialite) {
+        super(id, nom, prenom, dateNaissance, email, telephone);
+        this.specialite = specialite;
+    }
 }
