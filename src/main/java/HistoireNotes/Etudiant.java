bf133433f6cfcd92e17912fb7e11ae4ed403e73b
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 09:57:47 +0300

    add the class Etudiant

--- src/main/java/HistoireNotes/Etudiant.java
+++ src/main/java/HistoireNotes/Etudiant.java
@@ -1,4 +1,23 @@
 package HistoireNotes;
 
-public class Etudiant {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+
+public class Etudiant extends Personne {
+    private String groupe;
+    private Tuteur tuteur;
+    private List<Note> notes;
+
+    public Etudiant(int id, String nom, String prenom, LocalDate dateNaissance, String email, String telephone, String groupe, Tuteur tuteur, List<Note> notes) {
+        super(id, nom, prenom, dateNaissance, email, telephone);
+        this.groupe = groupe;
+        this.tuteur = tuteur;
+        this.notes = notes;
+    }
 }
